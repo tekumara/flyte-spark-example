04a463abe4614756818c0946b1097d33f752ab1e
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2021-08-25 20:49:50 +1000

    fix lints

--- fspark/main.py
+++ fspark/main.py
@@ -4,12 +4,11 @@ from operator import add
 
 import flytekit
 from flytekit import task, workflow
-
+from flytekitplugins.spark import Spark
 from pyspark.sql import SparkSession
 
-from flytekitplugins.spark import Spark
-
 # taken from https://docs.flyte.org/projects/cookbook/en/latest/auto/integrations/kubernetes/k8s_spark/pyspark_pi.html
+
 
 @task(
     task_config=Spark(
@@ -28,24 +27,24 @@ def hello_spark(partitions: int) -> float:
     print("Starting Spark with Partitions: {}".format(partitions))
 
     n = 100000 * partitions
-    sess:SparkSession = flytekit.current_context().spark_session
-    count = (
-        sess.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
-    )
+    sess: SparkSession = flytekit.current_context().spark_session
+    count = sess.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
     pi_val = 4.0 * count / n
     print("Pi val is :{}".format(pi_val))
     return pi_val
 
 
-def f(_):
+def f(_: int) -> int:
     x = random.random() * 2 - 1
     y = random.random() * 2 - 1
     return 1 if x ** 2 + y ** 2 <= 1 else 0
+
 
 @task(cache_version="1")
 def print_every_time(value_to_print: float, date_triggered: datetime.datetime) -> int:
     print("My printed value: {} @ {}".format(value_to_print, date_triggered))
     return 1
+
 
 @workflow
 def my_spark(triggered_date: datetime.datetime) -> float:
@@ -64,6 +63,4 @@ if __name__ == "__main__":
     may result in local runtime failures.
     """
     print(f"Running {__file__} main...")
-    print(
-        f"Running my_spark(triggered_date=datetime.datetime.now()){my_spark(triggered_date=datetime.datetime.now())}"
-    )
+    print(f"Running my_spark(triggered_date=datetime.datetime.now()){my_spark(triggered_date=datetime.datetime.now())}")
